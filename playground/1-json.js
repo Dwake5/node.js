d221728276d6735ab607fcb44118c89f4561e0c0
Danny Wakeling <wakeling101@hotmail.com>
2019-08-15 13:29:54 +0100

    json challenge done

--- playground/1-json.js
+++ playground/1-json.js
@@ -7,7 +7,20 @@ const fs = require('fs')
 // const bookJSON = JSON.stringify(book)
 // fs.writeFileSync('1-json.json', bookJSON)
 
+
+// 1. Load and parse the JSON data
 const dataBuffer = fs.readFileSync('1-json.json')
 const dataJSON = dataBuffer.toString()
-const data = JSON.parse(dataJSON)
-console.log(data.title)
+const user = JSON.parse(dataJSON)
+
+// 2. Change the name and age property to mine
+user.name = 'danny'
+user.age = 27
+
+// 3. Stringify it and overwrite the original
+const userJSON = JSON.stringify(user)
+fs.writeFileSync('1-json.json', userJSON)
+
+// 4. Test your work by viewing data in the JSON file
+// Working after terminal 'node 1-json.js' 
+
